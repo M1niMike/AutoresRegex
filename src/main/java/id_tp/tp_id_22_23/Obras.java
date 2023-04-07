172e93ff14fc4750166b4a21ee8248f936e3781a
Micael Eid <Mikaeleid@hotmail.com>
2023-04-07 15:44:14 +0100

    gg

--- src/main/java/id_tp/tp_id_22_23/Obras.java
+++ src/main/java/id_tp/tp_id_22_23/Obras.java
@@ -10,11 +10,16 @@ package id_tp.tp_id_22_23;
  */
 public class Obras {
     String isbn;
-    String codigo_autor;
+    //String codigo_autor; - Uma excepção
     String nome_autor;
     String titulo; 
     String editora;
     Double preco;
+    //Adições a maisQ
+    String quant_paginas;
+    String idioma;
+    String encadernacao;
+            
   //String  foto_capa;
     
 }
