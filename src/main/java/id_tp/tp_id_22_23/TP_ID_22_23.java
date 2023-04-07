ecca29ef10d74f1f76332fd95c483be827ac15d8
Micael Eid <Mikaeleid@hotmail.com>
2023-04-07 14:44:33 +0100

    Main: Init

---
+++ src/main/java/id_tp/tp_id_22_23/TP_ID_22_23.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package id_tp.tp_id_22_23;
+
+/**
+ *
+ * @author mikae
+ */
+public class TP_ID_22_23 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
