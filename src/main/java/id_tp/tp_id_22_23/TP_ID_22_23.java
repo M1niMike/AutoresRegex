2e7f557945cd15f4818186b366e222c5a5a4c367
Micael Eid <Mikaeleid@hotmail.com>
2023-04-07 15:00:00 +0100

    FT: main class

--- src/main/java/id_tp/tp_id_22_23/TP_ID_22_23.java
+++ src/main/java/id_tp/tp_id_22_23/TP_ID_22_23.java
@@ -4,13 +4,17 @@
 
 package id_tp.tp_id_22_23;
 
+import java.io.IOException;
+
 /**
  *
  * @author mikae
  */
 public class TP_ID_22_23 {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        //Prototipo de chamada
+        String a = Wrappers.obtem_titulo("9789892314044");
+        System.out.println(a);
     }
 }
